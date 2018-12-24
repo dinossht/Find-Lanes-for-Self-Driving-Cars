4b1d96bc26dfbbff43b4e87a7dbad57d88927e04
dinossht <dinosshan1996@hotmail.com>
2018-12-24 14:43:29 +0100

    Stop drawing lines if the line coordinates are out of image bound

--- lanes.py
+++ lanes.py
@@ -1,7 +1,6 @@
 # source: https://www.youtube.com/watch?v=eLTLtUVuuy4
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def canny(image):
@@ -19,6 +18,7 @@ def canny(image):
 def region_of_interest(image):
     height = image.shape[0]
     length = image.shape[1]
+    # Area of interest in the image
     polygons = np.array([
         [(0, height), (length, height), (int(length/2), int(height/2))]
     ])
@@ -29,6 +29,7 @@ def region_of_interest(image):
     # Fill the mask with our region of interest in white pixels
     mask = cv2.fillPoly(mask, pts=polygons, color=255)
 
+    # Overlay original image with the mask
     masked_image = cv2.bitwise_and(src1=image, src2=mask)
     return masked_image
 
@@ -40,33 +41,28 @@ def display_lines(image, lines):
         for line in lines:
             if line is not None:
                 x1, y1, x2, y2 = line.reshape(4)
-
-                cv2.line(line_image, pt1=(x1, y1), pt2=(x2, y2), color=(255, 0, 0), thickness=5)
+                # Check whether the coordinates are in inside the image, and draw
+                if (x1 >= 0 and x1 <= image.shape[1]) and (x2 >= 0 and x2 <= image.shape[1]) \
+                            and (y1 >= 0 and y1 <= image.shape[0]) and (y2 >= 0 and y2 <= image.shape[0]):
+                    cv2.line(line_image, pt1=(x1, y1), pt2=(x2, y2), color=(0, 255, 0), thickness=10)
 
     return line_image
 
 
 def make_coordinates(image, line_parameters):
+    # Create coordinates for the right and left lines on the road
     slope, intercept = line_parameters
     y1 = image.shape[0]
-    y2 = int(y1*(3/5))
+    y2 = int(y1*(3/4))
 
     x1 = int((y1 - intercept) / slope)
     x2 = int((y2 - intercept) / slope)
-    if x1 < 0:
-        x1 = 0
-    elif x1 > image.shape[1]:
-        x1 = image.shape[1] - 1
-
-    if x2 < 0:
-        x2 = 0
-    elif x2 > image.shape[1]:
-        x2 = image.shape[1] - 1
 
     return np.array([x1, y1, x2, y2])
 
 
 def average_slope_intercept(image, lines):
+    # Average out the lines slope calculate a single line on left and right side of the road
     left_fit = []
     right_fit = []
     left_line = None
@@ -78,11 +74,13 @@ def average_slope_intercept(image, lines):
             parameters = np.polyfit(x=(x1, x2), y=(y1, y2), deg=1)
             slope = parameters[0]
             intercept = parameters[1]
+            # Right side line has positive slope and vise versa
             if slope < 0:
                 left_fit.append((slope, intercept))
             else:
                 right_fit.append((slope, intercept))
 
+        # Check for whether a line exists
         if left_fit:
             left_fit_average = np.average(left_fit, axis=0)
             left_line = make_coordinates(image, left_fit_average)
@@ -126,14 +124,14 @@ cv2.waitKey(0)
 
 cap = cv2.VideoCapture("road_video.mp4")
 
-while(cap.isOpened()):
+while cap.isOpened():
     _, frame = cap.read()
-    print(frame.shape)
-    #resized_frame = frame[0:180, :]
     resized_frame = frame
+    #resized_frame = frame[:, 50:]
+
+    print(resized_frame.shape)
 
     canny_image = canny(resized_frame)
-
     cropped_image = region_of_interest(canny_image)
 
     # Identify lines in our image using Hough transform (y = mx+b => (m,b))
@@ -145,13 +143,14 @@ while(cap.isOpened()):
 
     line_image = display_lines(resized_frame, averaged_line)
 
-    combo_image = cv2.addWeighted(src1=resized_frame, alpha=1, src2=line_image, beta=0.8, gamma=1)
+    # Display lines on our original frame
+    combo_image = cv2.addWeighted(src1=resized_frame, alpha=1, src2=line_image, beta=1, gamma=1)
 
     cv2.imshow('result', combo_image)
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(10) == ord('q'):
         break
 
-
+# Close video file
 cap.release()
 cv2.destroyAllWindows()
 
